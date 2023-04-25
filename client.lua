c554a81fef923a427249cb9fec776894612badc6
M4XL1M1T <centsgames@gmail.com>
2023-04-25 19:11:07 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,27 +1,23 @@
-local CurrentDistance = 0
-local showTime = 0
+local CurDist = 0
+local dspTime = 0
 
 RegisterNetEvent('SaltyChat_VoiceRangeChanged')
 AddEventHandler('SaltyChat_VoiceRangeChanged', function(voiceRange, index, availableVoiceRanges)
-    CurrentDistance = voiceRange
-    showTime = 0
+    CurDist = voiceRange
+    dspTime = 0
+               
+    exports['okokNotify']:Alert('SaltyChat', 'Voicerange Now: ' .. voiceRange, 2000, 'info', true)
 end)
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(5)
-        if showTime < 25 then
-            local pedCoords = GetEntityCoords(PlayerPedId())
-            DrawMarker(1, pedCoords.x, pedCoords.y, pedCoords.z - 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, CurrentDistance * 1.3, CurrentDistance * 1.3, 0.5, 154, 1, 248, 110, false, false, 2, false, nil, nil, false)
-            showTime = showTime + 1
+        if dspTime < 25 then
+            local Coords = GetEntityCoords(PlayerPedId())
+            DrawMarker(1, Coords.x, Coords.y, Coords.z - 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, CurDist * 1.3, CurDist * 1.3, 0.5, 154, 1, 248, 110, false, false, 2, false, nil, nil, false)
+            dspTime = dspTime + 1
         else
-            Citizen.Wait(250)
+            Citizen.Wait(250) --Change Showtime
         end
     end
 end)
-
-AddEventHandler("SaltyChat_VoiceRangeChanged", function(voiceRange, index, availableVoiceRanges)
-    --Paste your Notify export here 
-     exports['okokNotify']:Alert('SaltyChat', 'Voicerange Now: ' .. voiceRange, 2000, 'info', true)
-    end)
-
